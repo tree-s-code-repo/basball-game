0f1f8c396866bac579ecebcdcf11c1b41567b50f
yunseorim1116 <hhyjs2r@gmail.com>
2023-11-28 16:07:53 +0900

    [feat] views 파일 수정

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync('입력 문구 작성');
-    // ...
+  async readNumber() {
+    const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    return input;
   },
-  // ...
+
+  async readAskNumber() {
+    const input = await Console.readLineAsync(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    );
+    return input;
+  },
 };
 
 export default InputView;
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,8 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printMenu() {
-    Console.print('출력 로직 작성');
+  printMessage(message) {
+    Console.print(message);
     // ...
   },
   // ...
