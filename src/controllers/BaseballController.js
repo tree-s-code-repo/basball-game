4258c8057419a0cc303c657509d96493c070e544
yunseorim1116 <hhyjs2r@gmail.com>
2023-11-28 16:39:06 +0900

    [feat] 컴퓨터 넘버 생성, 입출력 함수를 분리한다.

--- src/controllers/BaseballController.js
+++ src/controllers/BaseballController.js
@@ -5,36 +5,46 @@ import GameManager from '../models/GameManager.js';
 import OutputView from '../views/OutputView.js';
 
 class BaseballController {
-  userNumber;
+  #userNumbers;
+
+  #comNumbers;
 
   getComputerNumbers() {
     const computer = new Computer();
     const randomNumbers = computer.getRandomNumbers();
-    return randomNumbers;
+    console.log(randomNumbers);
+
+    this.setComputerNumbers(randomNumbers);
   }
 
   async promptNumbers() {
     const userInput = await InputView.readNumber();
     const numbers = userInput.split('').map(str => Number(str));
+
     this.setUserNumbers(numbers);
   }
 
+  setComputerNumbers(randomNumbers) {
+    this.#comNumbers = randomNumbers;
+  }
+
   setUserNumbers(numbers) {
-    this.userNumber = new UserNumber(numbers);
+    this.#userNumbers = new UserNumber(numbers).getNumbers();
   }
 
   async compare() {
-    const computerNumbers = this.getComputerNumbers();
-    const userNumbers = this.userNumber.getNumbers();
-    const gameManager = new GameManager(userNumbers, computerNumbers);
+    const gameManager = new GameManager(this.#userNumbers, this.#comNumbers);
     const gameResult = gameManager.getResult();
 
     this.printResult(gameResult);
 
-    if (gameResult === '3스트라이크') {
-      OutputView.printMessage('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      await this.askRestart();
+    if (gameResult !== '3스트라이크') {
+      await this.play();
+      return;
     }
+
+    OutputView.printMessage('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    await this.askRestart();
   }
 
   printResult(gameResult) {
@@ -44,16 +54,20 @@ class BaseballController {
   async askRestart() {
     const userInput = await InputView.readAskNumber();
 
-    if (userInput === 1) {
+    if (userInput === '1') {
       await this.start();
     }
   }
 
-  async start() {
-    OutputView.printMessage('숫자 야구 게임을 시작합니다.');
-
+  async play() {
     await this.promptNumbers();
     this.compare();
+  }
+
+  async start() {
+    this.getComputerNumbers();
+    OutputView.printMessage('숫자 야구 게임을 시작합니다.');
+    await this.play();
   }
 }
 export default BaseballController;
