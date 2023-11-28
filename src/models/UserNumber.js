a24fe8235264770d5cb7703b525c580d9da176dd
yunseorim1116 <hhyjs2r@gmail.com>
2023-11-28 14:43:31 +0900

    [feat] 유저가 입력한 number 를 관리하고 validtae 하는 model 을 생성한다.

---
+++ src/models/UserNumber.js
@@ -0,0 +1,40 @@
+class UserNumber {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validate(numbers) {
+    this.#validtaeLength(numbers);
+
+    numbers.forEach(number => {
+      this.#validateNumber(number);
+    });
+  }
+
+  #validtaeLength(numbers) {
+    if (numbers.length !== 3) {
+      throw new Error('[ERROR} 3개의 정수를 입력해주세요!');
+    }
+  }
+
+  #validateNumber(number) {
+    if (typeof number !== 'number') {
+      throw new Error('[ERROR} 숫자만 입력해주세요!');
+    }
+    if (Number.isNaN(number)) {
+      console.log('ㅠㅅㅠ');
+      throw new Error('[ERROR] 정수를 입력해주세요!');
+    }
+    if (!Number.isInteger(number)) {
+      throw new Error('[ERROR] 정수를 입력해주세요!');
+    }
+    if (number < 1) {
+      throw new Error('[ERROR] 양수를 입력해주세요!');
+    }
+  }
+}
+
+export default UserNumber;
